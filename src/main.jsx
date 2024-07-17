306e4b21726ce12b9c0f9d9689fa06a6ada49345
euluis-dev <fluisf00@gmail.com>
2024-07-17 12:32:39 -0300

    add feature: was imported the files in thethe files were imported in the Main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from './App.jsx'
+import App from './App.jsx';
 import PageHome from './pages/home/PageHome.jsx';
+import Calculator from './pages/calculator/Calculator.jsx';
+import TodoList from './pages/todoList/TodoList.jsx';
 
-import './index.css'
+import './index.css';
 
 
 ReactDOM.createRoot(document.querySelector('#root')).render(  
@@ -14,8 +16,8 @@ ReactDOM.createRoot(document.querySelector('#root')).render(
       <Routes>
         <Route element={<App />} >
           <Route path='/' element={<PageHome />} />
-          <Route path='/' element={<PageHome />} />
-          <Route path='/' element={<PageHome />} />
+          <Route path='calculator' element={<Calculator />} />
+          <Route path='todolist' element={<TodoList />} />
         </Route>
       </Routes>
     </BrowserRouter>
