9f6bd8e2c93a7a02200603c4dde4c4d86593d9b6
euluis-dev <fluisf00@gmail.com>
2024-07-17 12:13:33 -0300

    add feature: the routes feature was added, and was created PageHome.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.jsx'
+import PageHome from './pages/home/PageHome.jsx';
+
 import './index.css'
+
 
 ReactDOM.createRoot(document.querySelector('#root')).render(  
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />} >
-          <Route path='/' element={<App />} />
+          <Route path='/' element={<PageHome />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/home/PageHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageHome() {
+  return (
+    <div>
+      <h1>I am PageHome</h1>
+    </div>
+  )
+}
+
+export default PageHome;
