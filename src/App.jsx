c5e978aa999f38e513dfa493ef73bc80cd9470d3
euluis-dev <fluisf00@gmail.com>
2024-07-17 12:46:13 -0300

    add feature: the Links were created in the PageHome.jsx file -- navbar fictititous ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
     <div className="app">
+      <nav id='navbar'>
+        <h2>
+          <Link to='/'>Page Home</Link>
+        </h2>
+        <Link to='/calculator'>Calculator</Link>
+        <Link to='/todolist'>Todo List</Link>
+      </nav>
       <h1>conso.log Project</h1>
     </div>
   )
