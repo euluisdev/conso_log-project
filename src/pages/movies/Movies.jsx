fecc30a9b88f17a43d9bde0edb25c2494c5b50ca
euluis-dev <fluisf00@gmail.com>
2024-07-18 09:11:08 -0300

    add feature: create two pages

---
+++ src/pages/movies/Movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Movies() {
+  return (
+    <div>
+      <h1>Morning, i'm Movies</h1>
+    </div>
+  )
+}
+
+export default Movies;
---
+++ src/pages/stepsform/FormSteps.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FormSteps() {
+  return (
+    <div>
+      <h2>Let me splain guy, im Form Steps</h2>
+    </div>
+  )
+}
+
+export default FormSteps;
