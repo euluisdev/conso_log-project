7482edb32e483896050c37871e57235e09c8b3ed
euluis-dev <fluisf00@gmail.com>
2024-07-20 14:48:57 -0300

    create feature: text created

--- src/pages/home/PageHome.jsx
+++ src/pages/home/PageHome.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 function PageHome() {
   return (
-    <div>
-      <h1>I am PageHome</h1>
-    </div>
+    <section className='homePage-cont'>
+      <div className='initial-text'>
+        <h1>I am PageHome</h1>
+      </div>
+      <div>
+        <p>slider</p>
+      </div>
+      <div className="txt-end">
+        <p>
+          As imagens acima oferecem um resumo de alguns dos problemas diários enfrentado por nós, e nossa aplicação resolve cada um desses problemas de forma simples e direta, e  cada uma projetada para interatividade dinâmica. Você pode explorar todas essas aplicações em pleno funcionamento em nosso servidor, através da barra de navegação localizada no TOPO do site, e fixada. Nossa aplicação foi cuidadosamente desenvolvida para operar sem problemas ou bugs em qualquer tamanho de tela ou dispositivo, garantindo uma experiência incrível para você. Após verificar todas as funcionalidades, há um breve formulário, onde pode ser acessado na barra de menu fixada acima, e poderá avaliar toda a plicação, verificar sua avaliação, e enviar, para que possamos melhorar cada vez mais sua experiência com a aplicação.
+        </p>
+      </div>
+    </section>
   )
 }
 
