7e8e909d4cccfb2685ff4e48ff5945423b1beb35
euluis-dev <fluisf00@gmail.com>
2024-07-21 12:18:10 -0300

    create useEffet: type checking-- string empty

--- src/pages/home/HomeTitle.jsx
+++ src/pages/home/HomeTitle.jsx
@@ -4,6 +4,11 @@ const HomeTitle = ({ title }) => {
     const [displayedTitle, setDisplayedTitle] = useState('');
     const [showCursor, setShowCursor] = useState(!true);
     
+    useEffect(() => {
+        if (typeof title === 'string' &&  title !== '') {
+            let currentIndex = 0;
+        }
+    })
 
   return (
     <div>
