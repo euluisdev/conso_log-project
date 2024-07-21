da98e96c7c6d96af9ffc7724b1581cb50effe29a
euluis-dev <fluisf00@gmail.com>
2024-07-21 16:49:05 -0300

    add feature: Swiper Slider

--- src/pages/home/HomeSlider.jsx
+++ src/pages/home/HomeSlider.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const HomeSlider = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import 'swiper/css'; 
+ 
+const HomeSlider = () => { 
+    const data = [ 
+        { id: 1, text: 'Resolva seus cálculos matemáticos utilizando nossa calculadora.', src: 'src/pages/home/img/1+1=3.jpg' },
+        { id: 2, text: 'Organize sua lista de afazeres diários com a nossa ToDo list.', src: 'src/pages/home/img/home-todo.jpg' },
+        { id: 3, text: 'Viaje no mundo Cinematográfico, desbravando toda descrição do seu fime preferido.', src: 'src/pages/home/img/movie.jpg' },
+        { id: 4, text: 'Avalie toda nossa aplicação pelo nosso formulário incrível!', src: 'src/pages/home/img/form.jpg' },  
+    ]; 
 
-export default HomeSlider;
+
+    return ( 
+        <div className='slider-container'>  
+            <Swiper 
+                slidesPerView={1} 
+                pagination={{ clickable: true }} 
+                navigation 
+            > 
+                { 
+                    data.map((item) => ( 
+                        <SwiperSlide key={item.id}>
+                            <img src={item.src} alt="Slider" className='slide-item' />
+                            <h1 className='slide-txt'>{item.text}</h1>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+    ) 
+} 
+
+export default HomeSlider; 
