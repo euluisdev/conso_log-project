b45a6e7ef3d9e720438ae5c92b944ca0d3193251
euluis-dev <fluisf00@gmail.com>
2024-07-21 12:34:46 -0300

    create feature: setInterval was created to the show the title

--- src/pages/home/HomeTitle.jsx
+++ src/pages/home/HomeTitle.jsx
@@ -3,18 +3,29 @@ import { useState, useEffect } from "react";
 const HomeTitle = ({ title }) => {
     const [displayedTitle, setDisplayedTitle] = useState('');
     const [showCursor, setShowCursor] = useState(!true);
-    
-    useEffect(() => {
-        if (typeof title === 'string' &&  title !== '') {
-            let currentIndex = 0;
-        }
-    })
 
-  return (
-    <div>
-      <h1>{ title }</h1>
-    </div>
-  )
+    useEffect(() => {
+        if (typeof title === 'string' && title !== '') {
+            let currentIndex = 0;
+
+            const interval = setInterval(() => {
+                if (currentIndex <= title.length) {
+                    setDisplayedTitle(title.substring(0, currentIndex));
+                    currentIndex++
+                } else {
+                    clearInterval(interval);
+                }
+            }, 100);
+
+            return () => clearInterval(interval);
+        };
+    }, [title]);
+
+    return (
+        <div>
+            <h1>{displayedTitle}</h1>
+        </div>
+    )
 }
 
 export default HomeTitle;
