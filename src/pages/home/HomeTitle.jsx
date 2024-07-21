e96d50cee29aed62ad6cdbf91bbd242d0c10d0cd
euluis-dev <fluisf00@gmail.com>
2024-07-21 12:13:13 -0300

    create states: the string and bolean states were created

--- src/pages/home/HomeTitle.jsx
+++ src/pages/home/HomeTitle.jsx
@@ -1,6 +1,10 @@
-
+import { useState, useEffect } from "react";
 
 const HomeTitle = ({ title }) => {
+    const [displayedTitle, setDisplayedTitle] = useState('');
+    const [showCursor, setShowCursor] = useState(!true);
+    
+
   return (
     <div>
       <h1>{ title }</h1>
