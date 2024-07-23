878ee10b7f3612b9447d05ecae3d6a238eefb1fb
euluis-dev <fluisf00@gmail.com>
2024-07-23 15:40:03 -0300

    create feature: class=full-width was created

--- src/pages/calculator/Calculator.jsx
+++ src/pages/calculator/Calculator.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 function Calculator() {
   return (
-    <div>
-      <h1>Hi, im Calculator</h1>
+    <div className='cont-ainer'>
+      <div className="calculator-bg">
+        <section className="full-width">
+          <h2>Cópia simples da Calculadora iOS</h2>
+          <p>
+            Simplifique suas operações matemáticas com nossa nova aplicação React! 🧮 Com acesso rápido e fácil a uma calculadora intuitiva, você pode realizar cálculos básicos e avançados com apenas alguns cliques.
+            Precisa fazer uma adição, subtração, multiplicação ou divisão? Nossa calculadora cobre todas essas operações e muito mais.
+            Nosso design limpo e amigável torna a experiência de cálculo ainda mais agradável. Com uma interface intuitiva e botões de fácil acesso, você dominará a matemática em pouco tempo.
+          </p>
+        </section>
+        <section className='examples-tricts'>
+
+        </section>
+      </div>
     </div>
   )
 }
