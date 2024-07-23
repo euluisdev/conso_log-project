2b0d4b2d41aed87430a1d00db4c629c1fb025a95
euluis-dev <fluisf00@gmail.com>
2024-07-23 10:51:46 -0300

    add feature: the Footer component was imported in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { Outlet } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 import './App.css'
 
-function App() {
-  return (
-    <div className="app">
-      <NavBar />
-      <Outlet />
-    </div>
-  )
-}
+function App() { 
+  return (  
+    <> 
+      <div className="app"> 
+        <NavBar /> 
+        <Outlet />  
+      </div>
+      <footer>
+        <Footer /> 
+      </footer>
+    </>
+  ) 
+}; 
 
 export default App;
