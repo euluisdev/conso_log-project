cc6a9b6fbfdf4862c35b601dfe41c32f4a0adfd2
euluis-dev <fluisf00@gmail.com>
2024-07-23 15:55:01 -0300

    add feature: the class=flex-cont-ainer was created

--- src/pages/calculator/Calculator.jsx
+++ src/pages/calculator/Calculator.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Box, Container } from "@mui/material";
+
 
 function Calculator() {
   return (
@@ -16,8 +17,42 @@ function Calculator() {
 
         </section>
       </div>
-    </div>
-  )
-}
+      <section className='flex-cont-ainer'>
+        <div className="calculator">
+          <Box m={9} />
+          <Container maxWidth="xs">
+            <div className="wrapper">
+              <h3>Calculator</h3>
+              <h1 className="resOperation">23</h1>
+              <Box m={9} />
+              <button className="btn-all" onClick={clear}>AC</button>
+              <button className="btn-all" onClick={handlesReverseSignal}>+/-</button>
+              <button className="btn-all" onClick={percentage}>%</button>
+              <button className="btn-all orange" onClick={changeOperation}>/</button>
+              <button className="btn-all gray" onClick={updateScreen}>7</button>
+              <button className="btn-all gray" onClick={updateScreen}>8</button>
+              <button className="btn-all gray" onClick={updateScreen}>9</button>
+              <button className="btn-all orange" onClick={changeOperation}>*</button>
+              <button className="btn-all gray" onClick={updateScreen}>4</button>
+              <button className="btn-all gray" onClick={updateScreen}>5</button>
+              <button className="btn-all gray" onClick={updateScreen}>6</button>
+              <button className="btn-all orange" onClick={changeOperation}>-</button>
+              <button className="btn-all gray" onClick={updateScreen}>1</button>
+              <button className="btn-all gray" onClick={updateScreen}>2</button>
+              <button className="btn-all gray" onClick={updateScreen}>3</button>
+              <button className="btn-all orange" onClick={changeOperation}>+</button>
+              <button className="btn-all gray wide" onClick={updateScreen}>0</button>
+              <button className="btn-all gray" onClick={updateScreen}>.</button>
+              <button className="btn-all orange" onClick={processEqual}>=</button>
+            </div>
+          </Container >
+          <section className='history-calc'>
+            
+          </section> 
+        </div> 
+      </section> 
+    </div> 
+  ) 
+}; 
 
 export default Calculator;
