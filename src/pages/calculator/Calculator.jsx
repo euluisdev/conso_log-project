05bb10e930faecfaad2f8ea37096f209d2d06b60
euluis-dev <fluisf00@gmail.com>
2024-07-26 10:59:35 -0300

    add documentation: react documentation used

--- src/pages/calculator/Calculator.jsx
+++ src/pages/calculator/Calculator.jsx
@@ -39,18 +39,18 @@ const Calculator = () => {
 
   const CalculatorHistory = () => {
     return (
-    <div className="calculator-history">
-      <h2>Histórico de Cálculos</h2>
-      <ul>
-        {history.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
+      <div className="calculator-history">
+        <h2>Histórico de Cálculos</h2>
+        <ul>
+          {history.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
 
-   /* calculator start */
+  /* calculator start */
 
   function updateScreen(e) {
     const inputValue = e.target.innerText;
@@ -149,9 +149,37 @@ const Calculator = () => {
             <CalculatorHistory />
           </section>
         </div>
+
+        <div className="half-width highlighted-text">
+          <h2>/* Documentação React Utilizada: */</h2>
+          <p>
+            &nbsp;&nbsp;&nbsp;Durante o desenvolvimento desta aplicação em REACT, foram consultadas e aplicadas algumas seções da documentação oficial do React. Acesse cada parte da Documentação REACT, através do link em azul, que aparece nesta sessão:
+            <br />&nbsp;&nbsp;&nbsp;##Introdução ao JSX:
+            Utilizado para escrever a interface da calculadora de forma declarativa, permitindo a criação de elementos React utilizando uma sintaxe semelhante ao HTML.
+            <a href="https://reactjs.org/docs/jsx-in-depth.html"> JSX In Depth.  </a>
+            <br />&nbsp;&nbsp;&nbsp;##Componentes e Props:
+            A calculadora é dividida em componentes reutilizáveis, como Button, Display, CalculatorHistory, e Calculator. As props são usadas para passar dados e funções entre esses componentes.
+            <a href="https://reactjs.org/docs/components-and-props.html"> Components and Props.  </a>
+            <br></br>&nbsp;&nbsp;&nbsp;##Estado e Ciclo de Vida:
+            Utilizados para gerenciar o estado interno da calculadora. O estado é mantido em variáveis como num, oldNumber, fourOperation e history, que armazenam o valor atual, o valor antigo, a operação selecionada e o histórico de cálculos, respectivamente.
+            <a href="https://reactjs.org/docs/state-and-lifecycle.html"> State and Lifecycle.  </a>
+            <br></br>&nbsp;&nbsp;&nbsp;##Manipulação de Eventos:
+            Para capturar e tratar eventos de clique nos botões da calculadora, permitindo que a interface reaja às ações do usuário.
+            <a href="https://reactjs.org/docs/handling-events.html"> Handling Events.  </a>
+            <br></br>&nbsp;&nbsp;&nbsp;##Listas e Chaves:
+            São utilizadas para renderizar os botões da calculadora de forma eficiente, garantindo que cada botão tenha uma chave única, otimizando a renderização.
+            <a href="https://reactjs.org/docs/lists-and-keys.html"> Lists and Keys.  </a>
+            <br></br>&nbsp;&nbsp;&nbsp;##Hooks
+            useState é usado para gerenciar o estado local dos componentes funcionais, permitindo o controle dos valores e operações da calculadora de forma eficiente.
+            &nbsp;<a href="https://reactjs.org/docs/hooks-intro.html">Introducing Hooks.  </a>
+            &nbsp;&nbsp;<a href="https://reactjs.org/docs/hooks-state.html">Using the State Hook.  </a>
+            <br></br>&nbsp;&nbsp;&nbsp;##Este projeto está licenciado sob a licença MIT. Consulte o arquivo LICENSE no meu GITHUB para obter mais informações.
+          </p>
+        </div>
+
       </section>
     </div>
-  )
+  );
 };
 
 export default Calculator;
