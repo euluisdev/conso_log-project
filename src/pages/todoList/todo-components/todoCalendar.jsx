c2379c81964f320a0c1c296773414416533f1c3b
euluis-dev <fluisf00@gmail.com>
2024-07-29 09:00:21 -0300

    add feature: the state was created

--- src/pages/todoList/todo-components/todoCalendar.jsx
+++ src/pages/todoList/todo-components/todoCalendar.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css'
 
 
 const TodoCalendar = () => {
+    const [calendar, setCalendar] = useState(new Date);
+
   return (
     <div>
       <Calendar />
