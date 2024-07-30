7d62bf27e6659249b3532a4e8f9c5d7f63f515bd
euluis-dev <fluisf00@gmail.com>
2024-07-30 10:11:11 -0300

    material-ui item was adited according to the project

--- src/pages/todoList/todo-components/TodoItem.jsx
+++ src/pages/todoList/todo-components/TodoItem.jsx
@@ -8,54 +8,29 @@ import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import CommentIcon from '@mui/icons-material/Comment';
 
-const TodoItem =() => {
-  const [checked, setChecked] = React.useState([0]);
+const TodoItem = () => {
 
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  return (
-    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
-
-        return (
-          <ListItem
-            key={value}
+    return (
+        <ListItem
             secondaryAction={
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
+                <IconButton edge="end" aria-label="comments">
+                    <CommentIcon />
+                </IconButton>
             }
             disablePadding
-          >
-            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+        >
+            <ListItemButton role={undefined} dense>
+                <ListItemIcon>
+                    <Checkbox
+                        edge="start"
+                        tabIndex={-1}
+                        disableRipple
+                    />
+                </ListItemIcon>
+                <ListItemText primary={`Line item`} />
             </ListItemButton>
-          </ListItem>
-        );
-      })}
-    </List>
-  );
+        </ListItem>
+    );
 };
 
 export default TodoItem; 
