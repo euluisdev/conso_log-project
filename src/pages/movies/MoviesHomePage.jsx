9c7518371270b272ad3096d5dfc4b664bb4faada
euluis-dev <fluisf00@gmail.com>
2024-08-04 15:11:32 -0300

    add feature: developing jsx text

--- src/pages/movies/MoviesHomePage.jsx
+++ src/pages/movies/MoviesHomePage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+
 
 const MoviesHomePage = () => {
-  return (
-    <div>
-      <h1>Hi, im movies home page</h1>
-    </div>
-  )
-}
+    return (
+        <div className="movies-app">
+            <section className="double-text">
+                <section className="movies-txts half-width highlighted-text">
+                    <h2>Explore o Universo Cinematográfico</h2>
+                    <p className="text-styled">
+                        &nbsp;&nbsp;&nbsp;&nbsp;Descubra o universo cinematográfico em suas mãos com nossa nova aplicação React! Nossa aplicação foi desenvolvida utilizando as tecnologias mais modernas e robustas para garantir uma experiência de usuário fluida e eficiente. Imagine ter acesso a um vasto catálogo de filmes, todos ao alcance de um clique. Com nossa ferramenta, você pode explorar e pesquisar seus filmes favoritos em segundos, graças à integração perfeita com a API do <strong>TMDB</strong>. 
+                        <br />&nbsp;&nbsp;&nbsp;&nbsp;Quer saber mais sobre um filme específico? Sem problemas! Basta digitar o título do seu filme preferido em nossa barra de navegação abaixo, e nossa aplicação exibirá uma riqueza de filmes e informações, desde a imagem da capa até detalhes cruciais como avaliações, sinopse e muito mais.
+                        <br />&nbsp;&nbsp;&nbsp;&nbsp;Além disso, nossa interface amigável e intuitiva garante que você navegue facilmente por entre os filmes, tornando a experiência de descoberta ainda mais cativante.
+                        <br />&nbsp;&nbsp;&nbsp;&nbsp;Mais Detalhes para explorar o código-fonte completo desta aplicação, incluindo instruções de instalação e execução, visite meu repositório no GitHub, e no Arquivo README.md do projeto, você encontrará informações adicionais sobre a estrutura do projeto, funcionalidades implementadas e outros detalhes relevantes. 
+                        <br />&nbsp;&nbsp;&nbsp;&nbsp;Nesta página ainda encontrará os termos Técnicos da aplicação, e a mesma rodando no navegador. 
+                        <br />&nbsp;&nbsp;&nbsp;&nbsp;Então, por que esperar? Junte-se a nós e mergulhe no mundo do cinema com facilidade e estilo. Digite agora mesmo seu filme preferido e comece sua jornada cinematográfica! <span className="emoji">🍿</span> 
+                    </p>
+                </section>
+            </section>
+
+            <section className="nave-two">
+
+            </section>
+        </div>
+    );
+};
 
 export default MoviesHomePage;
