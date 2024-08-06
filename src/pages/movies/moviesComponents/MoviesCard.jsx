8ae9e7875f909d595a67e8a3acb8d4f3ccb88c3b
euluis-dev <fluisf00@gmail.com>
2024-08-06 20:00:12 -0300

    add feature: moviescard component was finalized

--- src/pages/movies/moviesComponents/MoviesCard.jsx
+++ src/pages/movies/moviesComponents/MoviesCard.jsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 
 const imageUrl = import.meta.env.VITE_IMG;
 
-const MoviesCard = ({ movie }) => {
+const MoviesCard = ({ movie, showLink = true }) => {
   return (
     <div className="movie card">
-      <img src={imageUrl + movie.poster_path} alt={movie.title} /> 
+      <img src={imageUrl + movie.poster_path} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>
         <FaStar /> {movie.vote_average}
       </p>
       {
-        <Link to={`/movie/${movie.id}`}>Detalhes</Link>
+        showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>
       }
     </div>
   )
 }
-
+ 
 export default MoviesCard;
 
