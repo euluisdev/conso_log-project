704be9e34d36efeed2e52063ed47c4dcd0edcfc6
euluis-dev <fluisf00@gmail.com>
2024-08-06 19:44:42 -0300

    add feature: img and title prop was passed to the return

--- src/pages/movies/moviesComponents/MoviesCard.jsx
+++ src/pages/movies/moviesComponents/MoviesCard.jsx
@@ -1,9 +1,11 @@
 
 const imageUrl = import.meta.env.VITE_IMG;
 
-const MoviesCard = () => {
+const MoviesCard = ({ movie }) => {
   return (
-    <div>
+    <div className="movie card">
+      <img src={imageUrl + movie.poster_path} alt={movie.title} /> 
+      <h2>{movie.title}</h2>
       <p>hi, im movies card component</p>
     </div>
   )
