e1dbf927e83d83907272e6999927f3323e889303
euluis-dev <fluisf00@gmail.com>
2024-08-06 15:24:34 -0300

    add feature: the getTopMovies function was created

--- src/pages/movies/moviesPages/MoviesBest.jsx
+++ src/pages/movies/moviesPages/MoviesBest.jsx
@@ -7,6 +7,13 @@ const apiKey = `${import.meta.env.VITE_API_KEY}`;
 const MoviesBest = () => {
     const [topMovies, setTopMovies] = useState([]);
 
+    const getTopMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.jason();
+
+        console.log(data);
+    }; 
+
   return (
     <div>
       <h1>Hi, im movies best</h1>
