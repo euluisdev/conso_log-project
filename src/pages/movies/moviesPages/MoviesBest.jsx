95188766db0d9ad6a59438ef1a1c54342834b27b
euluis-dev <fluisf00@gmail.com>
2024-08-06 15:20:43 -0300

    import state: new state was created

--- src/pages/movies/moviesPages/MoviesBest.jsx
+++ src/pages/movies/moviesPages/MoviesBest.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 
 
 const moviesUrl = `${import.meta.env.VITE_API}`;
 const apiKey = `${import.meta.env.VITE_API_KEY}`;
 
 const MoviesBest = () => {
+    const [topMovies, setTopMovies] = useState([]);
+
   return (
     <div>
       <h1>Hi, im movies best</h1>
