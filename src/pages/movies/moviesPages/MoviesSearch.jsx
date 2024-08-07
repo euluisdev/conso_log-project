1ca19342baad8ca30c39793b2fad4f6cfc40f589
euluis-dev <fluisf00@gmail.com>
2024-08-07 15:05:08 -0300

    add feature-- return was created

--- src/pages/movies/moviesPages/MoviesSearch.jsx
+++ src/pages/movies/moviesPages/MoviesSearch.jsx
@@ -2,8 +2,20 @@
 
 const MoviesSearch = () => {
   return (
-    <div>
-      <h1>Hi, im search</h1>
+    <div className="movies-container">
+      <h2 className="title">
+        Resultados para: <span className="query-text">{query}</span>
+      </h2>
+      <div className="moviesContainer">
+        {
+          movies.length === 0 && <p>Carregando...</p>
+        }
+        {
+          movies.length > 0 && movies.map((movie) =>
+            <MoviesCard key={movie.id} movie={movie} />
+          )
+        }
+      </div>
     </div>
   )
 }
