b856e6801a3e9ef612ee0f553cc9f51f93d81dd0
euluis-dev <fluisf00@gmail.com>
2024-08-07 10:40:00 -0300

    add feature: new state was created with the usestate

--- src/pages/movies/moviesComponents/moviesNavbar.jsx
+++ src/pages/movies/moviesComponents/moviesNavbar.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 
 const MoviesNavbar = () => {
+    const [search, setSearch] = useState(''); 
+
   return (
     <nav id="movies-navbar">
     <h2>
