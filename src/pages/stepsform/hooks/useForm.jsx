96536fba92f2035bef23b6e06d468c13a7bc7fb1
euluis-dev <fluisf00@gmail.com>
2024-08-09 20:41:10 -0300

    fix bub: steps parameter are default

--- src/pages/stepsform/hooks/useForm.jsx
+++ src/pages/stepsform/hooks/useForm.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 
 
-const useForm = () => {
+const useForm = (steps) => {
     const [currentStep, setCurrentStep] = useState(0);  
 
     const changeSteps = (i, e) => {  
         if (e) e.preventDefault();  
 
-        if (i < 0 || i > currentStep.length) return;  
+        if (i < 0 || i >= steps.length) return;  
 
         setCurrentStep(i);
     };  
 
   return {
-    currentStep, 
+    currentStep: currentStep, 
     currentComponent: steps[currentStep], 
-    changeSteps: changeSteps,
+    changeSteps: changeSteps,  
+    
   }
 };  
 
