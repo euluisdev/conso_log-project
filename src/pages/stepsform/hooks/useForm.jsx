0be12cf05339ef8b886a562624cba910ff881349
euluis-dev <fluisf00@gmail.com>
2024-08-09 19:20:09 -0300

    add feature: changeSteps function was created epassed as object

--- src/pages/stepsform/hooks/useForm.jsx
+++ src/pages/stepsform/hooks/useForm.jsx
@@ -2,11 +2,20 @@ import { useState } from "react";
 
 
 const useForm = () => {
-    const [currentStep, setCurrentStep] = useState(0);
+    const [currentStep, setCurrentStep] = useState(0);  
+
+    const changeSteps = (i, e) => {  
+        if (e) e.preventDefault();  
+
+        if (i < 0 || i > currentStep.length) return;  
+
+        setCurrentStep(i);
+    };  
 
   return {
     currentStep, 
     currentComponent: steps[currentStep], 
+    changeSteps: changeSteps,
   }
 };  
 
