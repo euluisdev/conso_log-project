36e6161ce1c4350eb3fa782e062e87eeff281d71
euluis-dev <fluisf00@gmail.com>
2024-08-10 10:46:41 -0300

    add feature: jsx almost ready

--- src/pages/stepsform/form-components/FormUser.jsx
+++ src/pages/stepsform/form-components/FormUser.jsx
@@ -1,11 +1,31 @@
 
 
-const FormUser = () => {
-  return (
-    <div>
-      <h1>User Form</h1>
-    </div>
-  )
-}
+const FormUser = () => { 
+  return (  
+    <div> 
+      <div className="form-control"> 
+      <label htmlFor="name">Nome:</label> 
+        <input 
+          type="text" 
+          name="name" 
+          id="name" 
+          placeholder="Digite seu nome" 
+          required 
+        />
+      </div> 
+      <div className="form-control"> 
+      <label htmlFor="email">E-mail:</label> 
+        <input  
+          type="email"  
+          name="email"  
+          id="email" 
+          placeholder="Digite seu melhor E-mail" 
+          required 
+        />  
+      </div> 
+    </div> 
+  );  
+};  
 
-export default FormUser;
+export default FormUser;  
+  
