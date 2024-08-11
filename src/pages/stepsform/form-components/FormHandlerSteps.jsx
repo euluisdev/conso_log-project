2b13b3e6df3980f377181807d28396eed1f4024f
euluis-dev <fluisf00@gmail.com>
2024-08-11 09:51:27 -0300

    add feature: the return from FormHandlerSteps jsx is being created

--- src/pages/stepsform/form-components/FormHandlerSteps.jsx
+++ src/pages/stepsform/form-components/FormHandlerSteps.jsx
@@ -2,10 +2,20 @@
 
 const FormHandlerSteps = ({ currentStep }) => {
   return (
-    <div>
+    <div className="steps">
+        <div className="step active">
+            <p>Identificação</p>
+        </div>
+        <div className="step">
+            <p>Avaliação</p>
+        </div>
+        <div className="step">
+            <p>Envio</p>
+        </div>
       
     </div>
-  )
-}
+  );
+};   
 
-export default FormHandlerSteps;
+export default FormHandlerSteps;  
+
