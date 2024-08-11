7d7fd1cf1e356d662e11d812d00fc0d87cca7c06
euluis-dev <fluisf00@gmail.com>
2024-08-11 15:44:29 -0300

    add feature: jsx is being created -- label - inputs

--- src/pages/stepsform/form-components/FormReview.jsx
+++ src/pages/stepsform/form-components/FormReview.jsx
@@ -11,6 +11,30 @@ const FormReview = () => {
             name="review" 
           />
           <p>Insatisfeito</p>
+        </label>  
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="neutral"
+            name="review"
+          />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="satisfied"
+            name="review"
+          />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="very_satisfied"
+            name="review"
+          />
+          <p>Muito Satisfeito</p>
         </label>
       </div>      
     </div> 
