b8527ac2e4144ed77300fd72cf3cde3e5972ee69
euluis-dev <fluisf00@gmail.com>
2024-08-12 13:05:35 -0300

    data props was passed to the formthanks.jsx file and emojidata object was created ans used

--- src/pages/stepsform/form-components/FormThanks.jsx
+++ src/pages/stepsform/form-components/FormThanks.jsx
@@ -5,7 +5,14 @@ import {
   BsFillEmojiSmileFill
 } from "react-icons/bs"; 
 
-const FormThanks = () => {
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfied: <BsFillEmojiHeartEyesFill />
+};
+
+const FormThanks = ({ data }) => {
   return (
     <div className="thanks-container" >
       <h2>Falta pouco</h2>
@@ -13,12 +20,14 @@ const FormThanks = () => {
         A sua opinião é muito importante para a avaliação da aplicação em geral, e assim que concluir, verá suas repostas em tempo real, e as mesmas serão enviadas para o autor da aplicação.    
       </p>  
       <p>Para concluir sua avaliação, clique no botão Enviar, abaixo.</p>   
-      <h3>Aqui está o resumo da sua avaliação: </h3>  
+      <h3>Aqui está o resumo da sua avaliação, {data.name}: </h3>  
       <p className="review-data">
         <span>Satisfação com o produto: </span>
+        {emojiData[data.review]}
       </p>
       <p className="review-data">  
         <span>Comentário: </span> 
+        {data.comment}
       </p> 
     </div>  
   );   
