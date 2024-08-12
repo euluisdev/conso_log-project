6bbb307e8fc0030e9f25d82d6b620c02035571ab
euluis-dev <fluisf00@gmail.com>
2024-08-12 09:54:45 -0300

    add feature: jsx almost finalized

--- src/pages/stepsform/form-components/FormThanks.jsx
+++ src/pages/stepsform/form-components/FormThanks.jsx
@@ -7,10 +7,22 @@ import {
 
 const FormThanks = () => {
   return (
-    <div>
-      <h1>Thanks Form</h1>      
-    </div>
-  )
-}
-
-export default FormThanks;
+    <div className="thanks-container" >
+      <h2>Falta pouco</h2>
+      <p>
+        A sua opinião é muito importante para a avaliação da aplicação em geral, e assim que concluir, verá suas repostas em tempo real, e as mesmas serão enviadas para o autor da aplicação.    
+      </p>  
+      <p>Para concluir sua avaliação, clique no botão Enviar, abaixo.</p>   
+      <h3>Aqui está o resumo da sua avaliação: </h3>  
+      <p className="review-data">
+        <span>Satisfação com o produto: </span>
+      </p>
+      <p className="review-data">  
+        <span>Comentário: </span> 
+      </p> 
+    </div>  
+  );   
+};    
+ 
+export default FormThanks;   
+ 
