88b03fcc2ab6ad9a81ab1e9523c612055cb9922e
euluis-dev <fluisf00@gmail.com>
2024-08-15 08:34:03 -0300

    add feature: formthanks was update

--- src/pages/stepsform/form-components/FormThanks.jsx
+++ src/pages/stepsform/form-components/FormThanks.jsx
@@ -1,9 +1,9 @@
-import { 
-  BsFillEmojiFrownFill, 
-  BsFillEmojiHeartEyesFill, 
-  BsFillEmojiNeutralFill, 
+import {
+  BsFillEmojiFrownFill,
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiNeutralFill,
   BsFillEmojiSmileFill
-} from "react-icons/bs"; 
+} from "react-icons/bs";
 
 const emojiData = {
   unsatisfied: <BsFillEmojiFrownFill />,
@@ -15,23 +15,24 @@ const emojiData = {
 const FormThanks = ({ data }) => {
   return (
     <div className="thanks-container" >
-      <h2>Falta pouco</h2>
+      <h1>Falta pouco</h1>
       <p>
-        A sua opinião é muito importante para a avaliação da aplicação em geral, e assim que concluir, verá suas repostas em tempo real, e as mesmas serão enviadas para o autor da aplicação.    
-      </p>  
-      <p>Para concluir sua avaliação, clique no botão Enviar, abaixo.</p>   
-      <h3>Aqui está o resumo da sua avaliação, {data.name}: </h3>  
-      <p className="review-data">
-        <span>Satisfação com o produto: </span>
-        {emojiData[data.review]}
+        A sua opinião é muito importante para a avaliação da aplicação em geral, e assim que concluir, verá suas repostas em tempo real, e as mesmas serão enviadas para o autor da aplicação.
       </p>
-      <p className="review-data">  
-        <span>Comentário: </span> 
-        {data.comment}
-      </p> 
-    </div>  
-  );   
-};    
- 
-export default FormThanks;   
- 
+      <p>Para concluir sua avaliação, clique no botão Enviar, abaixo.</p>
+      <div className="review-data">
+        <h3>Aqui está o resumo da sua avaliação, {data.name}: </h3>
+        <p>
+          <span>Satisfação com a aplicação: </span>
+          {emojiData[data.review]}
+        </p>
+        <p className="review-data">
+          <span>Seu comentário: </span>
+          {data.comment}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FormThanks;
