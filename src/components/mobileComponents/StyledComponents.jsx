8b114a0c245b50f0d6de8874c5008d45756d6f80
euluis-dev <fluisf00@gmail.com>
2024-08-16 12:01:19 -0300

    styledComponents.jsx file was finalized

--- src/components/mobileComponents/StyledComponents.jsx
+++ src/components/mobileComponents/StyledComponents.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ContainerMob = styled.section`
-  /* display: ${({ $isVisible }) => ($isVisible ? 'flex' : 'none')}; */
+  display: ${({ $isVisible }) => ($isVisible ? 'flex' : 'none')}; 
   color: blue;
 
   font-size: 2rem;
@@ -18,4 +18,14 @@ export const ContainerMob = styled.section`
   justify-content: center;
   position: fixed;
 
-`;
+  background-color: rgba(36, 36, 36, 0.836);
+
+  opacity: 0;
+  transition: opacity 0.2s;
+
+  ${({ $isVisible }) => $isVisible && css`
+    opacity: 1;
+    color: blue;
+  `} 
+`;  
+ 
