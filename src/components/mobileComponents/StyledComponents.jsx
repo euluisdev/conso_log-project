bc8551bfacbe8e57b0102aca3321cad85e3cda9d
euluis-dev <fluisf00@gmail.com>
2024-08-16 11:49:16 -0300

    add style: style is being applied to the StyledComponent.jsx file

--- src/components/mobileComponents/StyledComponents.jsx
+++ src/components/mobileComponents/StyledComponents.jsx
@@ -1,5 +1,21 @@
-import styled from "styled-components"; 
+import styled from "styled-components";
 
 export const ContainerMob = styled.section`
-  
+  /* display: ${({ $isVisible }) => ($isVisible ? 'flex' : 'none')}; */
+  color: blue;
+
+  font-size: 2rem;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(3px);
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 15rem 0 0 0;
+  z-index: 5;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+
 `;
