2ae4554ca14e4c13f6ccee27643a52d1dc26bd18
euluis-dev <fluisf00@gmail.com>
2024-08-16 10:19:09 -0300

    import file: NavBarMobile.jsx file was imported in the App.jsx file -- att bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import NavBarMobile from './components/mobileComponents/NavBarMobile';
 
 import './App.css'
 
 function App() { 
   return (  
-    <> 
+    <>
+    <NavBarMobile /> 
       <div className="app"> 
         <NavBar /> 
         <Outlet />  
