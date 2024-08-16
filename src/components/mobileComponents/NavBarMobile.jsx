b94d2d6574bef32785c6d1740986235d3db81305
euluis-dev <fluisf00@gmail.com>
2024-08-16 10:53:17 -0300

    add feature: li was finalized in the NavBarMobile.jsx file

--- src/components/mobileComponents/NavBarMobile.jsx
+++ src/components/mobileComponents/NavBarMobile.jsx
@@ -9,10 +9,10 @@ const NavBarMobile = ({ menuIsVisible, setMenuIsVisible }) => {
       <nav id="mob-nav"> 
         <ul>
           <li><Link to="/" onClick={() => setMenuIsVisible(false)}>CONSOLOG</Link></li> 
-          <li><Link to="/calculator"></Link></li> 
-          <li><Link to="/todo"></Link></li> 
-          <li><Link to="/movie"></Link></li>
-          <li><Link to="/form"></Link></li>
+          <li><Link to="/calculator" onClick={() => setMenuIsVisible(false)}>Calculator</Link></li> 
+          <li><Link to="/todo" onClick={() => setMenuIsVisible(false)}>TodoList</Link></li> 
+          <li><Link to="/movie" onClick={() => setMenuIsVisible(false)}>Movie</Link></li>
+          <li><Link to="/form" onClick={() => setMenuIsVisible(fale)}>FormSteps</Link></li>
         </ul> 
       </nav> 
     </div> 
