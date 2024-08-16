776c05dad3775c4c840128ae11cb9952b2407d2c
euluis-dev <fluisf00@gmail.com>
2024-08-16 10:24:10 -0300

    fix bug: ContainerMob component has not yet been developed in the navbarmobile.jsx file

--- src/components/mobileComponents/NavBarMobile.jsx
+++ src/components/mobileComponents/NavBarMobile.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
-import { ContainerMob } from './stylesComponent'; 
+/* import { ContainerMob } from './stylesComponent';  */
 
 
 const NavBarMobile = () => {
   return (
-    <ContainerMob className="mob-container"> 
+    <div className="mob-container"> 
       <nav id="mob-nav">
         <ul className="mob-logo">
           <li><Link to="/"></Link></li> 
         </ul>
       </nav>
-    </ContainerMob>
+    </div>
   );  
 };
 
