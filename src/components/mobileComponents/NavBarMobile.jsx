561c137f8b7afba31891204cdbc6c7738fe4f3dd
euluis-dev <fluisf00@gmail.com>
2024-08-16 10:46:04 -0300

    add feature: menuIsVisible, setMenuIsVisiblewere imported as prop to each Link

--- src/components/mobileComponents/NavBarMobile.jsx
+++ src/components/mobileComponents/NavBarMobile.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 /* import { ContainerMob } from './stylesComponent';  */
 
 
-const NavBarMobile = () => {
+const NavBarMobile = ({ menuIsVisible, setMenuIsVisible }) => {
   return (
     <div className="mob-container"> 
       <nav id="mob-nav"> 
         <ul>
-          <li><Link to="/"></Link></li> 
+          <li><Link to="/" onClick={() => setMenuIsVisible(false)}>CONSOLOG</Link></li> 
           <li><Link to="/calculator"></Link></li> 
           <li><Link to="/todo"></Link></li> 
           <li><Link to="/movie"></Link></li>
