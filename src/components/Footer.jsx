2d977d4dc97b6b346bd8e34a6063332dfd30451d
euluis-dev <fluisf00@gmail.com>
2024-08-17 21:13:18 -0300

    footer file: Link changed -- linkdin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,14 +11,14 @@ const Footer = () => {
     <footer className="footer-container">
         <div className="footer-icons">
             <ul>
-                <li><a href="https://github.com/euluisdev"><BsLinkedin /></a></li> 
+                <li><a href="https://www.linkedin.com/in/luis-figueiredo-077aba1a3/"><BsLinkedin /></a></li> 
                 <li><a href="https://github.com/euluisdev"><BsGithub /></a></li> 
                 <li><a href="https://github.com/euluisdev"><BsInstagram /></a></li> 
             </ul>
         </div>
         <div className="developed">
             <p>Desenvolvido por: <a href="https://github.com/euluisdev">euluis-dev</a></p> 
-            <p>Todos os direitor reservados: &copy; 2024 CONSO.LOG</p> 
+            <p>Todos os direitos reservados: &copy;2024 CONSO dot LOG</p> 
         </div>
     </footer>
   )
