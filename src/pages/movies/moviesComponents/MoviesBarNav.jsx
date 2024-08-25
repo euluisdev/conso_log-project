0f9bf898d109be05bb3d20287eed23dfe05c9ead
euluis-dev <fluisf00@gmail.com>
2024-08-25 13:04:28 -0300

    rebuilding moviesbarnav.jsx file

--- src/pages/movies/moviesComponents/MoviesBarNav.jsx
+++ src/pages/movies/moviesComponents/MoviesBarNav.jsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
 
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import { Link, useNavigate } from "react-router-dom";
 
 const MoviesBarNav = () => {
-  return (
-    <div>
-      I am nav
-    </div>
-  )
-}
+    const [search, setSearch] = useState('');
 
-export default MoviesBarNav;
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if (!search) return;
+
+        navigate(`search?q=${search}`);
+        setSearch('');
+    };
+
+    return (
+        <nav id="movies-navbar">
+            <h2>
+                <Link to=''>
+                    <BiCameraMovie /> CONSO.LOG Movies
+                </Link>
+            </h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Busque seu filme"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                    required
+                />
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav>
+    );
+};
+
+export default MoviesBarNav; 
