54f8b86c16e08eb2f336fc43a33f1348e7429896
euluis-dev <fluisf00@gmail.com>
2024-08-25 12:54:46 -0300

    homeslider.jsx file: the image were imported to the slider -- src rigth

--- src/pages/home/HomeSlider.jsx
+++ src/pages/home/HomeSlider.jsx
@@ -2,13 +2,18 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css'; 
 import './home.css/HomeSlider.css'
+
+import img1 from './img-home/1+1=3.jpg';
+import img2 from './img-home/home-todo.jpg';
+import img3 from './img-home/movie.jpg';
+import img4 from './img-home/form.jpg';
  
 const HomeSlider = () => { 
     const data = [ 
-        { id: 1, text: 'Resolva seus cálculos matemáticos utilizando nossa calculadora.', src: 'src/pages/home/img-home/1+1=3.jpg' }, 
-        { id: 2, text: 'Organize sua lista de afazeres diários com a nossa ToDo list.', src: 'src/pages/home/img-home/home-todo.jpg' }, 
-        { id: 3, text: 'Viaje no mundo Cinematográfico, desbravando toda descrição do seu fime preferido.', src: 'src/pages/home/img-home/movie.jpg' }, 
-        { id: 4, text: 'Avalie toda nossa aplicação pelo nosso formulário incrível!', src: 'src/pages/home/img-home/form.jpg' },   
+        { id: 1, text: 'Resolva seus cálculos matemáticos utilizando nossa calculadora.', src: img1 }, 
+        { id: 2, text: 'Organize sua lista de afazeres diários com a nossa ToDo list.', src: img2 }, 
+        { id: 3, text: 'Viaje no mundo Cinematográfico, desbravando toda descrição do seu fime preferido.', src: img3 }, 
+        { id: 4, text: 'Avalie toda nossa aplicação pelo nosso formulário incrível!', src: img4 },   
     ]; 
 
 
