d7f12851c1e7f1f762cece83ae8c94ef9f597f68
euluis-dev <fluisf00@gmail.com>
2024-09-02 10:48:04 -0300

    updated heigth of the form

--- src/pages/stepsform/form-components/FormUser.jsx
+++ src/pages/stepsform/form-components/FormUser.jsx
@@ -1,35 +1,34 @@
 
 
-const FormUser = ({ data, updateFieldHandler }) => { 
-  return (  
-    <div> 
-      <div className="form-control"> 
-      <label htmlFor="name">Nome:</label> 
-        <input 
-          type="text" 
-          name="name" 
-          id="name" 
-          placeholder="Digite seu nome" 
-          required 
-          value={data.name || ""}  
-          onChange={(e) => updateFieldHandler("name", e.target.value)}   
+const FormUser = ({ data, updateFieldHandler }) => {
+  return (
+    <div>
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite seu nome"
+          required
+          value={data.name || ""}
+          onChange={(e) => updateFieldHandler("name", e.target.value)}
         />
-      </div> 
-      <div className="form-control"> 
-      <label htmlFor="email">E-mail:</label> 
-        <input  
-          type="email"  
-          name="email"  
-          id="email" 
-          placeholder="Digite seu melhor E-mail" 
-          required 
-          value={data.email || ''} 
-          onChange={(e) => updateFieldHandler('email', e.target.value)}   
-        />  
-      </div> 
-    </div> 
-  );  
-};  
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite seu melhor E-mail"
+          required
+          value={data.email || ''}
+          onChange={(e) => updateFieldHandler('email', e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default FormUser;  
-  
+export default FormUser;
